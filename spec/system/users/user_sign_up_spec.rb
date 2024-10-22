551c4ec6ddf28bdc9353bd7051c3bd2954e79a13
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-22 20:20:22 -0300

    feat: cria teste de cadastro com sucesso

--- spec/system/users/user_sign_up_spec.rb
+++ spec/system/users/user_sign_up_spec.rb
@@ -32,4 +32,46 @@ describe 'Usuário que não é ainda cadastrado' do
     expect(page).to have_content 'Nome não pode ficar em branco'
     expect(page).to have_content 'Sobrenome não pode ficar em branco'
   end
+
+  it 'não consegue cadastro com email e cpf inválidos' do
+    # Arrange
+    # Act
+    visit root_path    
+    click_on 'Criar conta'
+    fill_in 'E-mail',	with: 'samuel@example' 
+    fill_in 'Senha',	with: '12345678912345'
+    fill_in 'Confirme sua senha',	with: '12345678912345'
+    fill_in 'CPF',	with: '12345678912345'
+    fill_in 'Nome',	with: 'Samuel'
+    fill_in 'Sobrenome',	with: 'Rocha'
+
+    click_on 'Cadastrar'
+    # Assert
+    expect(page).to have_content 'Não foi possível salvar usuário: 2 erros.'
+    expect(page).to have_content 'CPF deve ser um número válido'
+    expect(page).to have_content 'E-mail não é válido'
+  end
+
+  it 'cadastra-se com sucesso' do
+    # Arrange
+    cpf = CPF.generate.to_s
+    # Act
+    visit root_path    
+    click_on 'Criar conta'
+    fill_in 'E-mail',	with: 'samuel@example.com' 
+    fill_in 'Senha',	with: '12345678912345'
+    fill_in 'Confirme sua senha',	with: '12345678912345'
+    fill_in 'CPF',	with: cpf
+    fill_in 'Nome',	with: 'Samuel'
+    fill_in 'Sobrenome',	with: 'Rocha'
+
+    click_on 'Cadastrar'
+    # Assert
+    user = User.last
+    expect(page).to have_content 'Você realizou seu registro com sucesso'
+    within('header') do 
+      expect(page).to have_content user.description
+      expect(page).to have_button 'Sair'
+    end 
+  end
 end
