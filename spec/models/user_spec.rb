d633e6bccac6c011f6d139c939edc766ac104381
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-22 19:03:11 -0300

    feat: cria teste de verificação do método description

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'Método description da model de usuário' do
+    it 'exibe nome completo e email' do
+      # Arrange
+      user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com')
+      # Act
+      result = user.description
+      # Assert
+      expect(result).to eq 'Samuel Rocha - sam@hotmail.com'  
+    end
+  end
   context 'presence' do
     
   end
