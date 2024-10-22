589be51cfcd70625237b4702967b743f2e86a5aa
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-22 12:26:36 -0300

    feat: Verifica se um usuário logado consegue fazer logout

--- spec/system/users/user_sign_in_spec.rb
+++ spec/system/users/user_sign_in_spec.rb
@@ -10,7 +10,20 @@ describe 'Usuário acessa página de login' do
     fill_in "Senha",	with: "12345678910111"
     click_on 'Log in'
     # Assert
-    expect(current_path).to eq root_path
     expect(page).to have_content "#{user.first_name} #{user.last_name} - #{user.email}"
+  end
+
+  it 'e faz logout' do
+    # Arrange
+    user = User.create!(first_name: 'Samuel', last_name: 'Rocha', email: 'samuel@hotmail.com', password: '12345678910111',  cpf: '03466588911')
+    # Act
+    visit root_path
+    fill_in "E-mail",	with: "samuel@hotmail.com" 
+    fill_in "Senha",	with: "12345678910111"
+    click_on 'Log in'
+    click_on 'Sair'
+    # Assert
+    expect(page).not_to have_content "#{user.first_name} #{user.last_name} - #{user.email}"
+    expect(current_path).to eq new_user_session_path  
   end
 end
