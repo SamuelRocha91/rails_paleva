61c97d7f73904d6c68c9b6ed304231aaad6bf5b5
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-22 12:20:12 -0300

    feat: teste login de usuário e direcionamento

--- spec/system/users/user_sign_in_spec.rb
+++ spec/system/users/user_sign_in_spec.rb
@@ -8,7 +8,7 @@ describe 'Usuário acessa página de login' do
     visit root_path
     fill_in "E-mail",	with: "samuel@hotmail.com" 
     fill_in "Senha",	with: "12345678910111"
-    click_on 'Login'
+    click_on 'Log in'
     # Assert
     expect(current_path).to eq root_path
     expect(page).to have_content "#{user.first_name} #{user.last_name} - #{user.email}"
