ebaf71abd9a6783e4fea900c07b9431c552c5838
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-22 19:12:14 -0300

    feat: cria teste de validação de presença obrigatoria de campos no cadastro de usuário

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,53 @@ RSpec.describe User, type: :model do
       expect(result).to eq 'Samuel Rocha - sam@hotmail.com'  
     end
   end
-  context 'presence' do
+
+  describe 'Validações'
+    context 'presença obrigatória de' do
+      it 'email' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: '', password: '123456789101112', cpf: '03478599927')
+        # Act
+        result = user.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'first_name' do
+        # Arrange
+        user = User.new(first_name: '', last_name: 'Rocha', email: 'sam@hotmail.com', password: '123456789101112', cpf: '03478599927')
+        # Act
+        result = user.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'last_name' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: '', email: 'sam@hotmail.com', password: '123456789101112', cpf: '03478599927')
+        # Act
+        result = user.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'password' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com', password: '', cpf: '03478599927')
+        # Act
+        result = user.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'cpf' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com', password: '123456789101112', cpf: '')
+        # Act
+        result = user.valid?
+        # Assert
+        expect(result).to eq false
+      end
     
   end
   
