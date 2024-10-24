8d1193be90a7b6c251f7856b169c59469085db4a
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-24 20:50:32 -0300

    feat: gera migração de pratos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_23_220155) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_24_234951) do
+  create_table "dishes", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "calories"
+    t.integer "establishment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["establishment_id"], name: "index_dishes_on_establishment_id"
+  end
+
   create_table "establishments", force: :cascade do |t|
     t.string "trade_name"
     t.string "legal_name"
@@ -51,6 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_23_220155) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dishes", "establishments"
   add_foreign_key "establishments", "users"
   add_foreign_key "operating_hours", "establishments"
 end
--- spec/system/dishes/user_create_dishes_spec.rb
+++ spec/system/dishes/user_create_dishes_spec.rb
@@ -49,7 +49,12 @@ describe 'Usuário acessa formulário de criar pratos' do
     )
     operating_hour = []
     6.times { |i| operating_hour << OperatingHour.new(week_day: i, is_closed: true)}
-    operating_hour <<  OperatingHour.new(week_day: 6, start_time: Time.zone.parse('08:00'), end_time: Time.zone.parse('22:00'), is_closed: false)
+    operating_hour <<  OperatingHour.new(
+      week_day: 6, 
+      start_time: Time.zone.parse('08:00'), 
+      end_time: Time.zone.parse('22:00'), 
+      is_closed: false
+    )
     establishment.operating_hours = operating_hour
     establishment.save
     user.establishment = establishment 
