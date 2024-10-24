e8926b225baa51d235433d7f16d6e941a1e5bdbc
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-23 21:10:38 -0300

    feat: cria validações de cnpj, email e telefone

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -4,6 +4,30 @@ class Establishment < ApplicationRecord
   belongs_to :user
   has_many :operating_hours
   accepts_nested_attributes_for :operating_hours
+  validate :is_valid_cnpj?
+  validate :is_valid_phone_number?
+  validate :is_email_valid?
 
-  
+  private
+
+  def is_valid_cnpj?
+    if self.cnpj.present? && !(CNPJ.valid?(cnpj.to_i))
+      self.errors.add :cnpj, " deve ser um número válido"
+    end
+  end
+
+  def is_valid_phone_number?
+    if self.phone_number.present? && phone_number.match?(/\D/)
+      self.errors.add :phone_number, " deve ter apenas números"
+    elsif phone_number.length < 10 || phone_number.length > 11
+      self.errors.add :phone_number, " deve ser um número válido"
+    end
+  end
+
+  def is_email_valid?
+    regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    if self.email.present? && !email.match?(regex)
+      self.errors.add :email, " deve ser um número válido"
+    end
+  end
 end
--- spec/models/establishment_spec.rb
+++ spec/models/establishment_spec.rb
@@ -1,4 +1,109 @@
 require 'rails_helper'
 
 RSpec.describe Establishment, type: :model do
+  describe 'Validações' do
+    context 'presença obrigatória de' do
+      it 'email' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com')
+        establishment = Establishment.new(email:'', trade_name: 'Samsumg', legal_name: 'Samsumg LTDA', cnpj: '56924048000140', phone_number: '71992594946', address: 'Rua das Alamedas avenidas' )
+        establishment.user = user
+        # Act
+        result = establishment.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'trade_name' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com')
+        establishment = Establishment.new(email:'sam@hotmail.com', trade_name: '', legal_name: 'Samsumg LTDA', cnpj: '56924048000140', phone_number: '71992594946', address: 'Rua das Alamedas avenidas' )
+        establishment.user = user
+        # Act
+        result = establishment.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'legal_name' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com')
+        establishment = Establishment.new(email:'sam@hotmail.com', trade_name: 'Samsumg', legal_name: '', cnpj: '56924048000140', phone_number: '71992594946', address: 'Rua das Alamedas avenidas' )
+        establishment.user = user
+        # Act
+        result = establishment.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'cnpj' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com')
+        establishment = Establishment.new(email:'sam@hotmail.com', trade_name: 'Samsumg', legal_name: 'Samsumg LTDA', cnpj: '', phone_number: '71992594946', address: 'Rua das Alamedas avenidas' )
+        establishment.user = user
+        # Act
+        result = establishment.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'phone_number' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com')
+        establishment = Establishment.new(email:'sam@hotmail.com', trade_name: 'Samsumg', legal_name: 'Samsumg LTDA', cnpj: '56924048000140', phone_number: '', address: 'Rua das Alamedas avenidas' )
+        establishment.user = user
+        # Act
+        result = establishment.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'address' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com')
+        establishment = Establishment.new(email:'sam@hotmail.com', trade_name: 'Samsumg', legal_name: 'Samsumg LTDA', cnpj: '56924048000140', phone_number: '71992594946', address: '' )
+        establishment.user = user
+        # Act
+        result = establishment.valid?
+        # Assert
+        expect(result).to eq false
+      end
+    end
+
+    context 'deve ser válido' do
+      it 'cnpj' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com')
+        establishment = Establishment.new(email:'samsu@gmail.com', trade_name: 'Samsumg', legal_name: 'Samsumg LTDA', cnpj: '569240480140', phone_number: '71992594946', address: 'Rua das Alamedas avenidas' )
+        establishment.user = user
+        # Act
+        result = establishment.valid?
+        # Assert
+        expect(result).to eq false   
+      end
+
+      it 'phone_number' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com')
+        establishment = Establishment.new(email:'samsu@gmail.com', trade_name: 'Samsumg', legal_name: 'Samsumg LTDA', cnpj: '56924048000140', phone_number: '71992594a46', address: 'Rua das Alamedas avenidas' )
+        establishment.user = user
+        # Act
+        result = establishment.valid?
+        # Assert
+        expect(result).to eq false   
+      end
+
+
+      it 'email' do
+        # Arrange
+        user = User.new(first_name: 'Samuel', last_name: 'Rocha', email: 'sam@hotmail.com')
+        establishment = Establishment.new(email:'samsu@gmail', trade_name: 'Samsumg', legal_name: 'Samsumg LTDA', cnpj: '56924048000140', phone_number: '71992594946', address: 'Rua das Alamedas avenidas' )
+        establishment.user = user
+        # Act
+        result = establishment.valid?
+        # Assert
+        expect(result).to eq false   
+      end
+    end
+  end
 end
