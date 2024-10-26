6e7a48077a9d596472a437297b98244cd28a5ca7
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-25 23:28:27 -0300

    feat: busca um prato pelo nome

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,13 @@ class SearchController < ApplicationController
   
   def search
     if params[:type] == 'Bebida'
-      @beverages = Beverage.where('name LIKE ? or description LIKE ? ', "%#{params[:query]}%",  "%#{params[:query]}%")
+      @beverages = Beverage.where('name LIKE ? or description LIKE ? ',
+                                   "%#{params[:query]}%",  "%#{params[:query]}%")
       @count = @beverages.length
+    elsif params[:type] == 'Comida'
+      @dishes = Dish.where('name LIKE ? or description LIKE ? ',
+                                   "%#{params[:query]}%",  "%#{params[:query]}%")
+      @count = @dishes.length
     end
   end
 end
