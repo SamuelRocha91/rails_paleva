20a4ce2cbcb3e1d7ed8c860df8c24ac0d7bdd708
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-25 21:24:58 -0300

    feat: valida que um usuário não pode editar bebida de outro usuário

---
+++ spec/requests/user_edit_beverages_request_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+describe 'Usuário edita uma bebida' do 
+  it 'e não é o dono' do 
+    # Arrange
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572'
+    )
+
+    user_two = User.create!(
+      first_name: 'Bill', 
+      last_name: 'Gates', 
+      email: 'ng@hotmail.com', 
+      password: '12345678910111',  
+      cpf: CPF.generate
+    )
+
+    establishment = Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+      user: user
+    )
+
+    Establishment.create!(
+      email: 'bill@gmail.com', 
+      trade_name: 'Microsoft', 
+      legal_name: 'Microsoft LTDA', 
+      cnpj: '12345678000195',
+      phone_number: '71992594950', 
+      address: 'Rua da Microsoft',
+      user: user_two
+    )
+
+
+    beverage = Beverage.create!(name: 'cachaça', description: 'alcool delicioso baiano', 
+                 calories: '185', establishment: establishment, is_alcoholic: true)
+    # Act
+    login_as(user_two)
+    patch(establishment_dish_path(establishment.id, beverage.id), params: { beverage: { name: 'Coca-cola'}})
+    # Assert
+    expect(response).to redirect_to(root_path)
+  end
+end
--- spec/system/beverages/user_edit_beverages_spec.rb
+++ spec/system/beverages/user_edit_beverages_spec.rb
@@ -69,4 +69,53 @@ describe 'Usuário edita uma bebida' do
     expect(page).not_to have_content 'cachaça'
     expect(page).not_to have_content 'alcool delicioso baiano'
   end
+
+  it 'Caso seja o responsável' do
+    # Arrange
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572'
+    )
+
+    user_two = User.create!(
+      first_name: 'Bill', 
+      last_name: 'Gates', 
+      email: 'ng@hotmail.com', 
+      password: '12345678910111',  
+      cpf: CPF.generate
+    )
+
+    establishment = Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+      user: user
+    )
+
+    Establishment.create!(
+      email: 'bill@gmail.com', 
+      trade_name: 'Microsoft', 
+      legal_name: 'Microsoft LTDA', 
+      cnpj: '12345678000195',
+      phone_number: '71992594950', 
+      address: 'Rua da Microsoft',
+      user: user_two
+    )
+
+    beverage = Beverage.create!(name: 'cachaça', description: 'alcool delicioso baiano', 
+                 calories: '185', establishment: establishment, is_alcoholic: true)
+    # Act
+    login_as user_two
+    visit establishment_dish_path(establishment.id, beverage.id)
+
+    # Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Você não possui acesso a esse prato'
+  end
 end
