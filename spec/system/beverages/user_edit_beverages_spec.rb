9d0638b79c0b29fc2733cb47dc9817878425308f
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-25 21:05:39 -0300

    feat: verifica que o usuário deve estar autenticado pra editar bebida

---
+++ spec/system/beverages/user_edit_beverages_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'Usuário edita uma bebida' do
+  it 'e deve estar autenticado' do
+    # Arrange
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572'
+    )
+
+    establishment = Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+      user: user
+    )
+
+    beverage = Beverage.create!(name: 'cachaça', description: 'alcool delicioso baiano', 
+                 calories: '185', establishment: establishment, is_alcoholic: true)
+    
+    # Act
+    visit edit_establishment_beverage_path(establishment.id, beverage.id)
+    # Assert
+    expect(current_path).to eq new_user_session_path  
+  end
+end
