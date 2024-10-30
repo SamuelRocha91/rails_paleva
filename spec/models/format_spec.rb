b7611d98b82a031cee68149d7b47655a658899ff
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-30 19:55:32 -0300

    feat: cria testes de validação de campos obrigatórios

--- spec/models/format_spec.rb
+++ spec/models/format_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Format, type: :model do
+  describe 'Validação de formato' do
+    context 'Presença obrigatória de' do
+      it 'name' do
+        # Arrange
+        format = Format.new
+        # Act
+        result = format.valid?
+        # Assert
+        expect(result).to eq false  
+      end
+    end
+  end
 end
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -1,4 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Offer, type: :model do
+  describe 'Validação de oferta' do
+    context 'Presença obrigatória de' do
+      it 'price' do
+        # Arrange
+        user = User.create!(
+          first_name: 'Samuel', 
+          last_name: 'Rocha', 
+          email: 'samuel@hotmail.com', 
+          password: '12345678910111',  
+          cpf: '22611819572'
+        )
+        establishment = Establishment.create!(
+          email: 'sam@gmail.com', 
+          trade_name: 'Samsung', 
+          legal_name: 'Samsung LTDA', 
+          cnpj: '56924048000140',
+          phone_number: '71992594946', 
+          address: 'Rua das Alamedas avenidas',
+          user: user
+        )
+        dish = Dish.create!(
+          name: 'lasagna', 
+          description: 'massa, queijo e presunto', 
+          calories: '185', 
+          establishment: establishment
+        )
+        format = Format.create!(name: 'Porção grande')
+        offer = Offer.new(
+          format: format,
+          item: dish,
+          active: true,
+        )
+        # Act
+        result = offer.valid?
+        # Assert
+        expect(result).to eq false
+      end
+    end
+  end
 end
