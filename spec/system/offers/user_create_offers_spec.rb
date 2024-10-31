65c54452b43b46a4b87fb3a87c0babc45699662e
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-10-31 10:51:09 -0300

    feat: cria teste de verificação da necessidade de estar autenticado pra ceditar e criar ofertas de pratos

--- spec/system/offers/user_create_offers_spec.rb
+++ spec/system/offers/user_create_offers_spec.rb
@@ -1,6 +1,38 @@
 require 'rails_helper'
 
 describe 'Usuário acessa formulário de criar oferta' do
+  it 'e deve estar autenticado' do
+    # Arrange
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572'
+    )
+    establishment = Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+      user: user
+    )
+    dish = Dish.create!(
+      name: 'lasagna', 
+      description: 'massa, queijo e presunto', 
+      calories: '185', 
+      establishment: establishment
+    )
+
+    # Act
+    visit offer_dish_path(dish.id)
+
+    # Assert
+    expect(current_path).to eq new_user_session_path
+  end
+  
   it 'e vê os campos corretamente' do
     # Arrange
     user = User.create!(
--- spec/system/offers/user_edit_offers_spec.rb
+++ spec/system/offers/user_edit_offers_spec.rb
@@ -1,4 +1,41 @@
 describe 'Usuário acessa formulário de edição de oferta' do
+    it 'e deve estar autenticado' do
+    # Arrange
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572'
+    )
+    establishment = Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+      user: user
+    )
+    dish = Dish.create!(
+      name: 'lasagna', 
+      description: 'massa, queijo e presunto', 
+      calories: '185', 
+      establishment: establishment
+    )
+    format = Format.create!(name: 'Porção Giga gante')
+    offer = Offer.create!(
+      format: format,
+      item: dish,
+      price: 25
+    )
+    # Act
+    visit edit_offer_dish_path(dish.id, offer.id)
+
+    # Assert
+    expect(current_path).to eq new_user_session_path
+  end
+
   it 'e atualiza preço com sucesso' do
     # Arrange
     user = User.create!(
