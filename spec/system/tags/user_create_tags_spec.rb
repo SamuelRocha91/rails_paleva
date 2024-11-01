7c13bb4d663ee0677c8275cb608a696e703fdaf9
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-01 17:04:54 -0300

    feat: adiciona mensagem ilustrativa para caso de inexistencia de marcadores

--- spec/system/tags/user_create_tags_spec.rb
+++ spec/system/tags/user_create_tags_spec.rb
@@ -43,4 +43,32 @@ describe "Usuário acessa página de marcadores" do
     expect(page).to have_content 'Japonesa'
     expect(page).to have_content 'Massas'
   end
+
+  it 'e visualiza mensagem instrutiva se não houver tags cadastradas' do
+    # Arrange
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572'
+    )
+    Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+      user: user
+    )
+
+    # Act
+    login_as user
+    visit root_path
+    click_on 'Marcadores'
+    # Assert
+    expect(page).to have_content 'Marcadores Disponíveis'
+    expect(page).to have_content 'Não existem ainda marcadores cadastrados'
+  end
 end
