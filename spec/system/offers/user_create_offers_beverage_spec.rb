0d2bf0f1819b6c385515bd27b39b921dde2293bb
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-01 00:16:13 -0300

    feat: impossibilidade de cadastrar volumes de bebidas com mesmo nome

--- spec/system/offers/user_create_offers_beverage_spec.rb
+++ spec/system/offers/user_create_offers_beverage_spec.rb
@@ -177,12 +177,20 @@ describe 'Usuário acessa formulário de criar oferta de uma bebida' do
       address: 'Rua das Alamedas avenidas',
       user: user
     )
-    Beverage.create!(
+    format = Format.create!(name: 'Bombinha 50ml')
+
+    beverage = Beverage.create!(
       name: 'Cachaça', 
       description: 'alcool delicioso baiano', 
       calories: '185', 
       establishment: establishment, 
       is_alcoholic: true
+    )
+
+    Offer.create!(
+      format: format,
+      item: beverage,
+      price: 25
     )
 
     # Act
@@ -199,6 +207,52 @@ describe 'Usuário acessa formulário de criar oferta de uma bebida' do
     # Assert
     expect(page).to have_content 'Volume cadastrado com sucesso'
     expect(page).to have_content 'Volume Bombinha 1l: R$ 500,00'
+  end
+
+  it 'falha ao tentar cadastrar volume de mesmo nome de outro ja existente para um mesmo prato' do
+    # Arrange
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572'
+    )
+    establishment = Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+      user: user
+    )
+    beverage = Beverage.create!(
+      name: 'Cachaça', 
+      description: 'alcool delicioso baiano', 
+      calories: '185', 
+      establishment: establishment, 
+      is_alcoholic: true
+    )
+    format = Format.create!(name: 'Bombinha 50ml')
+    Offer.create!(
+      format: format,
+      item: beverage,
+      price: 25
+    )
+    # Act
+    login_as user
+    visit root_path
+    click_on 'Bebidas'
+    click_on 'Cachaça'
+    click_on 'Cadastrar volume'
+    fill_in 'Detalhes do volume',	with: 'bombinha de um só'
+    fill_in 'Nome do volume',	with: 'Bombinha 50ml'
+    fill_in 'Preço',	with: '500'
+    click_on 'Salvar'
+
+    # Assert
+    expect(page).to have_content 'Não é possível cadastrar volumes idênticos para a mesma bebida'
   end
 
 end
