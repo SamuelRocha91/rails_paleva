4f3cc489e56bfe1646e63682d798938589e5982b
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-01 17:48:08 -0300

    feat: cria validação de que só há um nome de tag e é case insensitive

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validações' do
+    context 'presença obrigatória de' do
+      it 'name' do
+        # Arrange
+        tag = Tag.new
+
+        # Act
+        result = tag.valid?
+
+        # Assert
+        expect(result).to eq false
+      end
+    end
+    context 'unicidade de campo' do
+      it 'name' do
+        # Arrange
+        Tag.create!(name: 'Apimentado')
+        format = Tag.new(name: 'Apimentado')
+
+        # Act
+        result = format.valid?
+
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'que é case insensitive' do
+        # Arrange
+        Tag.create!(name: 'Apimentado')
+        tag = Tag.new(name: 'ApimeNTAdo')
+
+        # Act
+        result = tag.valid?
+
+        # Assert
+        expect(result).to eq false
+      end
+    end
+  end
 end
