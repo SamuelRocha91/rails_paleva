09c303de4b7a5c10dcd421070da98eb40e66731d
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-06 18:23:46 -0300

    feat: cria form de cadastro de menu

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,5 +2,6 @@ class MenusController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @menu = Menu.new
   end
 end
--- spec/system/menus/user_create_menu_spec.rb
+++ spec/system/menus/user_create_menu_spec.rb
@@ -8,4 +8,34 @@ describe 'Usuário acessa formulário de cadastro de cardápio' do
     # Assert
     expect(current_path).to eq new_user_session_path
   end
+
+  it 'e visualiza campos corretamente' do
+    # Arrange
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572'
+    )
+
+    establishment = Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+      user: user
+    )
+
+    # Act
+    login_as user
+    visit root_path
+    click_on 'Cadastrar cardápio'
+    save_page
+    # Assert
+    expect(page).to have_field 'Nome do Cardápio'
+    expect(page).to have_button 'Salvar'
+  end
 end
