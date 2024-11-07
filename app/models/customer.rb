65ca640a310047c0f39035536bfad20eeab56cd7
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-07 11:12:22 -0300

    feat: validação de email, cpf e phone_numbe de customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,8 @@
 class Customer < ApplicationRecord
   validate :required_field
+  validate :valid_email?
+  validate :is_valid_cpf?
+  validate :is_valid_phone_number?
 
   private
 
@@ -7,6 +10,27 @@ class Customer < ApplicationRecord
     if (!self.email || self.email.empty?) && 
          (!self.phone_number || self.phone_number.empty?)
       self.errors.add :base, 'E-mail ou telefone deve ser preenchido'
+    end
+  end
+
+  def valid_email?
+    regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    if self.email && !self.email.match?(regex)
+       self.errors.add :email, 'E-mail deve ser válido'
+    end
+  end
+
+  def is_valid_cpf?
+    if self.cpf.present? && self.cpf.length > 0 && !CPF.valid?(cpf)
+      self.errors.add :cpf, ' deve ser um número válido'
+    end
+  end
+
+  def is_valid_phone_number?
+    if self.phone_number.present? && self.phone_number.length > 0
+      if self.phone_number.length > 11 || self.phone_number.length < 10
+        self.errors.add :phone_number, ' deve ser um número válido'
+      end
     end
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -15,7 +15,11 @@ RSpec.describe Customer, type: :model do
 
     it 'presença de email sem phone_number é válido' do
       # Arrange
-      customer = Customer.new(name: 'Samuel', cpf: CPF.generate, email: 'samuel_s@gmail.com' )
+      customer = Customer.new(
+        name: 'Samuel', 
+        cpf: CPF.generate, 
+        email: 'samuel_s@gmail.com' 
+      )
 
       # Act
       result = customer.valid?
@@ -26,13 +30,64 @@ RSpec.describe Customer, type: :model do
 
     it 'presença de phone_number sem email é válido' do
       # Arrange
-      customer = Customer.new(name: 'Samuel', cpf: CPF.generate, phone_number: '71992594946' )
+      customer = Customer.new(
+        name: 'Samuel', 
+        cpf: CPF.generate, 
+        phone_number: '71992594946' 
+      )
 
       # Act
       result = customer.valid?
 
       # Assert
       expect(result).to eq true
+    end
+
+    it 'caso presente, email deve ser válido' do
+      # Arrange
+      customer = Customer.new(
+        name: 'Samuel', 
+        cpf: CPF.generate, 
+        email: 'samuel_s@gm', 
+        phone_number: '71992594946' 
+      )
+
+      # Act
+      result = customer.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+
+    it 'caso presente, cpf deve ser válido' do
+      # Arrange
+      customer = Customer.new(
+        name: 'Samuel', 
+        cpf: '00', 
+        email: 'samuel_s@gmail.com',
+        phone_number: '71992594946' 
+      )
+
+      # Act
+      result = customer.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+
+    it 'caso presente, phone_number deve ser válido' do
+      # Arrange
+      customer = Customer.new(
+        name: 'Samuel', 
+        email: 'samuel_s@gmail.com', 
+        phone_number: '719925949' 
+      )
+
+      # Act
+      result = customer.valid?
+
+      # Assert
+      expect(result).to eq false
     end
   end
 end
