538996aa7f66d703e525fb84db7401f39098dcf0
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-08 09:34:15 -0300

    feat: valida falha de cadastro de pedido por ausencia de campo obrigatorio ou valido

--- spec/system/orders/user_create_orders_spec.rb
+++ spec/system/orders/user_create_orders_spec.rb
@@ -270,4 +270,64 @@ describe 'Usuário cadastra um pedido' do
     expect(page).to have_button 'Salvar'
   end
 
+  it 'falha ao tentar vincular cliente ao pedido' do
+    # Arrange
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572'
+    )
+
+    establishment = Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+      user: user
+    )
+    menu = Menu.create!(establishment: establishment, name: 'Café da manhã')
+    Menu.create!(establishment: establishment, name: 'Almoço')
+
+    dish = Dish.create!(
+          name: 'lasagna', 
+          description: 'massa, queijo e presunto', 
+          calories: '185', 
+          establishment: establishment
+    )
+
+    format = Format.create!(name: 'Porção grande')
+    format_two = Format.create!(name: 'Porção média')
+
+    Offer.create!(
+      format: format,
+      item: dish,
+      price: 55
+    )
+    Offer.create!(
+      format: format_two,
+      item: dish,
+      price: 25
+    )
+
+    MenuItem.create!(item: dish, menu: menu)
+
+    # Act
+    login_as user
+    visit root_path
+    click_on 'Café da manhã'
+    find('.Porção-grande-lasagna').click
+    fill_in 'Observação',	with: 'Sem cebola' 
+    click_on 'Adicionar ao Pedido'
+    click_on 'Finalizar Pedido'
+    fill_in 'E-mail',	with: ''
+    fill_in 'Nome',	with: 'Samuel'
+    click_on 'Salvar'
+    # Assert
+    expect(page).to have_content 'E-mail ou telefone deve ser preenchido'
+  end
+
 end
