c44569fada0a793a721a105001eaed8efb280f1f
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-08 12:59:58 -0300

    feat: pagina de visualização individual de pedido

--- spec/system/orders/user_view_order_details_spec.rb
+++ spec/system/orders/user_view_order_details_spec.rb
@@ -2,8 +2,28 @@ require 'rails_helper'
 
 describe 'Usuário acompanha um pedido' do
   it 'e deve estar autenticado' do
-    # ACT
-    visit orders_path
+    # Arrange
+    user = User.create!(
+        first_name: 'Samuel', 
+        last_name: 'Rocha', 
+        email: 'samuel@hotmail.com', 
+        password: '12345678910111',  
+        cpf: '22611819572'
+    )
+    establishment = Establishment.create!(
+        email: 'sam@gmail.com', 
+        trade_name: 'Samsung', 
+        legal_name: 'Samsung LTDA', 
+        cnpj: '56924048000140',
+        phone_number: '71992594946', 
+        address: 'Rua das Alamedas avenidas',
+        user: user
+    )
+    customer = Customer.create!(name: 'Samuel', email: 'sam@gmail.com')
+    order = Order.create!(establishment: establishment, customer: customer)
+    # Act
+    visit order_path order.id
+
     # Assert
     expect(current_path).to eq new_user_session_path  
   end
@@ -39,7 +59,7 @@ describe 'Usuário acompanha um pedido' do
     format = Format.create!(name: 'Porção grande')
 
     order = Order.create!(establishment: establishment, customer: customer)
-    order_two = Order.create!(establishment: establishment, customer: customer_two)
+    Order.create!(establishment: establishment, customer: customer_two)
 
     offer = Offer.create!(
       format: format,
@@ -53,19 +73,18 @@ describe 'Usuário acompanha um pedido' do
     login_as user
     visit root_path
     click_on 'Pedidos'
+    click_on order.code
 
     # Assert
-    within('table') do
-      expect(page).to have_content 'Acompanhamento de pedidos'
-      expect(page).to have_content 'Data'
-      expect(page).to have_content 'Preço'    
-      expect(page).to have_content 'Pedido'
-      expect(page).to have_content 'Cliente'
-      expect(page).to have_content 'Status'
-      expect(page).to have_content 'R$ 55,00'
-      expect(page).to have_content "#{order.code}"
-      expect(page).to have_content "Aguardando confirmação da cozinha"
-      expect(page).to have_content "#{order_two.code}"
-    end 
+    expect(page).to have_content 'Pedido'
+    expect(page).to have_content 'Dados do cliente'
+    expect(page).to have_content 'Nome do cliente: Samuel'    
+    expect(page).to have_content 'E-mail: sam@gmail.com'
+    expect(page).to have_content 'Porção grande - R$ 55,00'
+    expect(page).to have_content 'Nome do prato: lasagna'
+    expect(page).to have_content 'Status: Aguardando confirmação da cozinha'
+    expect(page).to have_content 'Total: R$ 55,00'
+    expect(page).to have_link 'Cancelar Pedido'
+    expect(page).to have_link 'Preparar Pedido'
   end
 end
