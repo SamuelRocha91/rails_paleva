bfa52681ce6f7a07c17c862e419220662ea6121c
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-12 19:41:58 -0300

    feat: possibilita passar um parametro por query para filtrar os pedidos do estabelecimento por status

--- app/controllers/api/v1/establishments_controller.rb
+++ app/controllers/api/v1/establishments_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::EstablishmentsController < ActionController::API
   def list_orders
     orders = Establishment.find_by(code: params[:code]).orders
+    orders = orders.where(status: params[:status]) if params[:status]
     render status: 200, json: orders.as_json(include: :customer, except: [:updated_at, :id]) 
   end
 end
--- spec/requests/api/establishment_orders_api_spec.rb
+++ spec/requests/api/establishment_orders_api_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Orders API' do
   context 'GET /api/v1/establishment/:code/orders/' do
-    it 'sem status, lista todos os pedidos do estabelecimento' do
+    it 'lista todos os pedidos do estabelecimento' do
       # Arrange
       establishment = Establishment.create!(
         email: 'sam@gmail.com', 
@@ -55,6 +55,56 @@ describe 'Orders API' do
       expect(json_response[1]["status"]).to eq 'in_preparation'
       expect(json_response[0].keys).not_to include 'updated_at'
       expect(json_response[0]["customer"]["name"]).to eq 'Samuel'
+    end
+
+    it 'filtra os pedidos do estabelecimento por status' do
+      # Arrange
+      establishment = Establishment.create!(
+        email: 'sam@gmail.com', 
+        trade_name: 'Samsung', 
+        legal_name: 'Samsung LTDA', 
+        cnpj: '56924048000140',
+        phone_number: '71992594946', 
+        address: 'Rua das Alamedas avenidas',
+      )
+      User.create!(
+        first_name: 'Samuel', 
+        last_name: 'Rocha', 
+        email: 'samuel@hotmail.com', 
+        password: '12345678910111',  
+        cpf: '22611819572',
+        establishment: establishment
+      )
+
+      customer = Customer.create!(name: 'Samuel', email: 'sam@gmail.com')
+      customer_two = Customer.create!(name: 'Ana', email: 'ana@gmail.com')
+
+      dish = Dish.create!(
+            name: 'lasagna', 
+            description: 'massa, queijo e presunto', 
+            calories: '185', 
+            establishment: establishment
+      )
+      format = Format.create!(name: 'Porção grande')
+
+      order = Order.create!(establishment: establishment, customer: customer)
+      order_two = Order.create!(establishment: establishment, customer: customer_two)
+      order_two.in_preparation!
+      offer = Offer.create!(
+        format: format,
+        item: dish,
+        price: 55
+      )
+      OrderItem.create!(offer: offer, order: order )
+
+      # Act
+      get "/api/v1/establishment/#{establishment.code}/orders?status=in_preparation"
+      # Assert
+      expect(response.status).to eq(200)
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq 1
+      expect(json_response[0]["code"]).to eq order_two.code
+      expect(json_response[0]["status"]).to eq 'in_preparation'
     end
   end
   
