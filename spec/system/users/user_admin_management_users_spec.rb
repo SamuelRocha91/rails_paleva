506b7adf257de4e2f3bbda392cf0637253301e59
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-18 20:08:05 -0300

    feat: insere funcionalidade de acompanhamento de usuários de um estabelecimento

--- spec/system/users/user_admin_management_users_spec.rb
+++ spec/system/users/user_admin_management_users_spec.rb
@@ -51,6 +51,7 @@ describe "Usuário faz pré-cadastro de funcionários" do
     visit root_path
 
     click_on establishment.trade_name
+    click_on 'Gerenciar usuários do estabelecimento'
     click_on 'Fazer pré-cadastro de usuário'
     fill_in 'E-mail',	with: 'mariazinha@gmail' 
     fill_in 'CPF',	with: '03466798507'
@@ -85,6 +86,7 @@ describe "Usuário faz pré-cadastro de funcionários" do
     visit root_path
 
     click_on establishment.trade_name
+    click_on 'Gerenciar usuários do estabelecimento'
     click_on 'Fazer pré-cadastro de usuário'
     fill_in 'E-mail',	with: 'mariazinha@gmail.com' 
     fill_in 'CPF',	with: '22611819572'
@@ -120,6 +122,7 @@ describe "Usuário faz pré-cadastro de funcionários" do
     visit root_path
 
     click_on establishment.trade_name
+    click_on 'Gerenciar usuários do estabelecimento'
     click_on 'Fazer pré-cadastro de usuário'
     fill_in 'E-mail',	with: 'mariazinha@gmail.com' 
     fill_in 'CPF',	with: '03466798507'
@@ -128,6 +131,57 @@ describe "Usuário faz pré-cadastro de funcionários" do
 
     # Assert
     expect(page).to have_content 'Pré-cadastro realizado com sucesso' 
+  end
+
+  it 'e visualiza estado de usuários vinculados ao seu estabelecimento' do
+    # Arrange
+    establishment = Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+    )
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572',
+      establishment: establishment
+    )
+
+    temporary_user = TemporaryUser.create!(
+      email: 'temporario@gamail.com',
+      cpf: CPF.generate,
+      establishment: establishment
+    )
+
+    user_two = User.create!(
+      first_name: 'Empregado', 
+      last_name: 'salario', 
+      email: 'empregado@hotmail.com', 
+      password: '12345678910111',  
+      cpf: CPF.generate,
+      establishment: establishment,
+      role: 1
+    )
+
+    # Act
+    login_as user
+    visit root_path
+
+    click_on establishment.trade_name
+    click_on 'Gerenciar usuários do estabelecimento'
+
+    # Assert
+    expect(page).to have_content 'empregado@hotmail.com'
+    expect(page).to have_content user_two.cpf
+    expect(page).to have_content 'Cadastrado' 
+    expect(page).to have_content 'temporario@gamail.com'
+    expect(page).to have_content temporary_user.cpf
+    expect(page).to have_content 'Pré-cadastrado' 
   end
 
 end
