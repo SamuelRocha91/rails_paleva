0024021c40c9475e9b37835d68f86e51ce653319
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-17 21:18:42 -0300

    fix: apaga conflitos e adiciona recomendações de uso do ruby

--- db/migrate/20241109120841_update_user_and_establishment_relationship.rb
+++
@@ -1,7 +0,0 @@
-class UpdateUserAndEstablishmentRelationship < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :establishments, :user_id, :bigint if column_exists?(:establishments, :user_id)
-
-    add_reference :users, :establishment, foreign_key: true, index: true
-  end
-end
