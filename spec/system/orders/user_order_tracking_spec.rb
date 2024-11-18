67c9fc9a42556260a4e56db5e44b36e55e4d663c
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-18 18:53:01 -0300

    feat: impõe obrigatoriedade de prato ou bebida pertencerem a um cardápio para serem adicionados a um pedido

--- spec/system/orders/user_order_tracking_spec.rb
+++ spec/system/orders/user_order_tracking_spec.rb
@@ -47,6 +47,11 @@ describe 'Usuário acompanha um pedido' do
       item: dish,
       price: 55
     )
+    menu = Menu.create!(
+      establishment: establishment, 
+      name: 'Café da manhã'
+    )
+    MenuItem.create!(item: dish, menu: menu)
 
     OrderItem.create!(offer: offer, order: order )
 
