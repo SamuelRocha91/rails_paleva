4edeb99c7d47c7c1d88462d620615959a4c2d484
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-17 21:43:29 -0300

    fix: correção de modelagem de tabelas frente a conflitos existentes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_13_020501) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_18_004231) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -156,9 +156,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_13_020501) do
     t.integer "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "establishment_id"
     t.integer "status", default: 0
     t.string "code"
+    t.integer "establishment_id", null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["establishment_id"], name: "index_orders_on_establishment_id"
   end
@@ -189,10 +189,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_13_020501) do
     t.string "cpf"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 0
     t.integer "establishment_id"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["establishment_id"], name: "index_users_on_establishment_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
