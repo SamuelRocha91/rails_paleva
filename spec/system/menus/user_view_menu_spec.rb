39af545c550efdeda08ee9d8f171726721ceaaea
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-11-21 10:21:27 -0300

    feat: valida visualização de menus na página inicial

--- spec/system/menus/user_view_menu_spec.rb
+++ spec/system/menus/user_view_menu_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Usuário acessa página de visualização individual de cardápio' do
+describe 'Usuário acessa página de visualização de cardápio' do
   it 'e deve estar autenticado' do
     # Arrange
    establishment = Establishment.create!(
@@ -30,6 +30,76 @@ describe 'Usuário acessa página de visualização individual de cardápio' do
 
     # Assert
     expect(current_path).to eq new_user_session_path
+  end
+
+  it 'e visualiza menus' do
+    # Arrange
+    establishment = Establishment.create!(
+      email: 'sam@gmail.com', 
+      trade_name: 'Samsung', 
+      legal_name: 'Samsung LTDA', 
+      cnpj: '56924048000140',
+      phone_number: '71992594946', 
+      address: 'Rua das Alamedas avenidas',
+    )
+    user = User.create!(
+      first_name: 'Samuel', 
+      last_name: 'Rocha', 
+      email: 'samuel@hotmail.com', 
+      password: '12345678910111',  
+      cpf: '22611819572',
+      establishment: establishment
+    )
+
+    menu = Menu.create!(
+      establishment: establishment, 
+      name: 'Café da manhã'
+    )
+
+    beverage = Beverage.create!(
+      name: 'Cachaça', 
+      description: 'alcool delicioso baiano', 
+      calories: '185', 
+      establishment: establishment, 
+      is_alcoholic: true
+    )
+    format = Format.create!(name: 'Bombinha 50ml')
+    Offer.create!(
+      format: format,
+      item: beverage,
+      price: 25
+    )
+
+    dish = Dish.create!(
+      name: 'lasagna', 
+      description: 'massa, queijo e presunto', 
+      calories: '185', 
+      establishment: establishment
+    )
+    format_two = Format.create!(name: 'Giga gante')
+    format_three = Format.create!(name: 'média')
+    Offer.create!(
+      format: format_two,
+      item: dish,
+      price: 27
+    )
+    Offer.create!(
+      format: format_three,
+      item: dish,
+      price: 12
+    )
+
+    MenuItem.create!(item: dish, menu: menu)
+    MenuItem.create!(item: beverage, menu: menu)
+
+    # Act
+    login_as user
+    visit root_path
+
+    # Assert
+    expect(page).to have_link 'Café da manhã'
+    expect(page).to have_content 'Prato: lasagna'
+    expect(page).to have_content 'Bebida: Cachaça'
   end
 
   it 'e visualiza pratos com itens e porções' do
